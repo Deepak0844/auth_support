526e1964f4d6b617bd0a39a25357e722b31acee4
Deepak0844 <deepakram0105@gmail.com>
2021-12-25 20:54:06 +0530

    auth message

---
+++ src/Activate.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Result } from "antd";
+
+export function Activate() {
+  const { token } = useParams();
+  const [data, setData] = useState();
+  useEffect(() => {
+    axios
+      .get(`http://localhost:9000/user/account-verification/${token}`)
+      .then((res) => {
+        console.log(res.data.message);
+      })
+      .catch((err) => {
+        console.log(err.response.data);
+      });
+  }, []);
+  return (
+    <div>
+      <Result
+        status="success"
+        title="Congratulations"
+        subTitle="Your account has been activated successfully."
+      />
+
+    </div>
+  );
+}
---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { PasswordVerify } from "./resetPassword";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { Activate } from "./Activate";
+import {PassChanged} from './passwordChanged';
+import 'antd/dist/antd.css';
+function App() {
+  return (
+    <div className="App">
+      <Switch>
+        <Route exact path="/"></Route>
+        <Route
+          exact
+          path="/forgot-password/verify/:token"
+          component={PasswordVerify}
+        />
+        <Route exact path="/accountverification/:token" component={Activate} />
+        <Route exact path="/passwordchangedsuccessfully" component={PassChanged} />
+      </Switch>
+    
+    </div>
+  );
+}
+
+export default App;
